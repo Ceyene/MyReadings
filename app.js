c13751347c0dd0a1747af6509cd886dd52105447
Ceyene <cyn.jaz.romero@gmail.com>
2021-05-21 17:13:36 -0300

    Connecting to mongodb new database with mongoose

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 const app = express();
+
+//connect to mongodb database
+mongoose.connect(
+  "mongodb+srv://ceyuser:ZCtdukH3742oSv13@mybooksapp.9r8kq.mongodb.net/myBooksApp?retryWrites=true&w=majority"
+);
+//Event listener: once the connection is open, it runs a callback
+mongoose.connection.once("open", () => {
+  console.log("Connected to database");
+});
 
 app.use(
   "/graphql",
