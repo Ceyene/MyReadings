206fcefc5408b1026f67d3b840eb4335a296f3e7
Ceyene <cyn.jaz.romero@gmail.com>
2021-05-21 17:29:41 -0300

    Creating Mongoose Models for authors and books data, and eliminating dummy data in schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require("graphql");
 const _ = require("lodash");
+const Book = require("../models/book");
+const Author = require("../models/author");
 
 const {
   GraphQLObjectType,
@@ -10,28 +12,6 @@ const {
   GraphQLList,
 } = graphql;
 
-//dummy data
-let books = [
-  { name: "Elantris", genre: "Fantasy", id: "1", authorId: "1" },
-  { name: "Mort", genre: "Fantasy", id: "2", authorId: "2" },
-  { name: "The Metamorphosis", genre: "Terror", id: "3", authorId: "3" },
-  { name: "Final del juego", genre: "Short Stories", id: "4", authorId: "4" },
-  { name: "Pride and Prejudice", genre: "Romance", id: "5", authorId: "5" },
-  { name: "The Call of Cthulhu", genre: "Terror", id: "6", authorId: "6" },
-  { name: "Equal Rites", genre: "Comedy", id: "7", authorId: "2" },
-  { name: "Mistborn", genre: "Fantasy", id: "8", authorId: "1" },
-  { name: "Rayuela", genre: "Classic", id: "9", authorId: "4" },
-];
-
-let authors = [
-  { name: "Brandon Sanderson", age: "44", id: "1" },
-  { name: "Terry Pratchett", age: "100", id: "2" },
-  { name: "Franz Kafka", age: "200", id: "3" },
-  { name: "Julio Cortázar", age: "300", id: "4" },
-  { name: "Jane Austen", age: "400", id: "5" },
-  { name: "H.P.Lovecraft", age: "600", id: "6" },
-];
-
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
@@ -41,7 +21,7 @@ const BookType = new GraphQLObjectType({
     author: {
       type: AuthorType,
       resolve(parent, args) {
-        return _.find(authors, { id: parent.authorId });
+        //return _.find(authors, { id: parent.authorId });
       },
     },
   }),
@@ -56,7 +36,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return _.filter(books, { authorId: parent.id });
+        //return _.filter(books, { authorId: parent.id });
       },
     },
   }),
@@ -71,26 +51,26 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         //code to get data from db/ other source
-        return _.find(books, { id: args.id });
+        //return _.find(books, { id: args.id });
       },
     },
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(authors, { id: args.id });
+        //return _.find(authors, { id: args.id });
       },
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return books;
+        //return books;
       },
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
-        return authors;
+        //return authors;
       },
     },
   },
