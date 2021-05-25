a3e99d6be8ef0b5503a474222aeb2c636b9077a9
Ceyene <cyn.jaz.romero@gmail.com>
2021-05-24 21:25:12 -0300

    Adding basic jsx structure and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./assets/styles/App.scss";
-import imagen from "./assets/static/reading-book.svg";
+import logo from "./assets/static/reading-book.svg";
 
 const App = () => (
-  <div>
-    <h1 className="title">Hello React!!!</h1>
-    <img src={imagen} alt="imagen" />
-  </div>
+  <>
+    <header className="navbar">
+      <img src={logo} alt="imagen" />
+      <h3>My Readings</h3>
+    </header>
+    <main className="main" id="main">
+      <div className="container">
+        <h1 className="main__title">My Readings List</h1>
+      </div>
+    </main>
+    <footer className="footer">
+      <p>2021 - Coded with ♥ by Cynthia Romero</p>
+    </footer>
+  </>
 );
 
 export default App;
